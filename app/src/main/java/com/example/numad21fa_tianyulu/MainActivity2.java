fe318e4e44cb5ae4a28fe209e1d0f0d4c6fb59e8
TimLu7 <90731993+TimLu7@users.noreply.github.com>
2021-09-29 22:42:54 -0700

    ButtonA function created

--- app/src/main/java/com/example/numad21fa_tianyulu/MainActivity2.java
+++ app/src/main/java/com/example/numad21fa_tianyulu/MainActivity2.java
@@ -3,12 +3,53 @@ package com.example.numad21fa_tianyulu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private Button btnE;
+    private Button btnF;
+    private TextView view2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btnA = findViewById(R.id.button3);
+        btnB = findViewById(R.id.button4);
+        btnC = findViewById(R.id.button5);
+        btnD = findViewById(R.id.button6);
+        btnE = findViewById(R.id.button7);
+        btnF = findViewById(R.id.button8);
+
+        btnA.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    view2.setText("Pressed: A");
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    view2.setText("Pressed: -");
+                }
+                return false;
+            }
+        });
+
+
+
+
+
     }
 }
