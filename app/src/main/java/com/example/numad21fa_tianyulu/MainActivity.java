6f2b3570723bfe970f09c4413b4e2b729a4db7ef
TimLu7 <90731993+TimLu7@users.noreply.github.com>
2021-09-29 22:27:09 -0700

    clicky button opens new activity

--- app/src/main/java/com/example/numad21fa_tianyulu/MainActivity.java
+++ app/src/main/java/com/example/numad21fa_tianyulu/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.numad21fa_tianyulu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,21 +10,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button toastButton;
-    Button Clicky;
+    private Button toastButton;
+    private Button clickyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        clickyBtn = findViewById(R.id.button2);
+
         toastButton = findViewById(R.id.button);
+
         toastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Tianyu Lu, lu.tianyu@northeastern.edu",Toast.LENGTH_LONG).show();
 
             }
+
+        });
+
+        clickyBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this,MainActivity2.class);
+            startActivity(intent);
         });
 
 
