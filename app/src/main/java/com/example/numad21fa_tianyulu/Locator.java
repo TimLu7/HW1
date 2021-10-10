58a13a395add0f9e7a32f62e78efa54801544233
TimLu7 <90731993+TimLu7@users.noreply.github.com>
2021-10-10 14:12:59 -0700

    locator activity created

--- app/src/main/java/com/example/numad21fa_tianyulu/Locator.java
+++ app/src/main/java/com/example/numad21fa_tianyulu/Locator.java
@@ -1,4 +1,24 @@
 package com.example.numad21fa_tianyulu;
 
-public class Locator {
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Locator extends AppCompatActivity {
+
+    private LocationManager locationmanager;
+    private String enableGPS = "Please enable GPS to get your location";
+    private String loadGPS = "GPS loading...";
+    private String location = "Latitude: %s \n Longtitude: %s";
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locator);
+    }
+
+
+
+
 }
